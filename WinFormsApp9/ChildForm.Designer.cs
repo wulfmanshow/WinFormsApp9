07586db6adbb41af330b4ee85a54278b1f199759
Greater-stack <63789742+Greater-stack@users.noreply.github.com>
2022-11-20 16:28:40 +0200

    Текст

--- WinFormsApp9/ChildForm.Designer.cs
+++ WinFormsApp9/ChildForm.Designer.cs
@@ -33,6 +33,12 @@ namespace MdiApplication
             this.ChildWindowMenu = new System.Windows.Forms.MenuStrip();
             this.ТoggleMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.formatToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pastToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ChildWindowMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,7 +48,7 @@ namespace MdiApplication
             this.ChildTextBox.Location = new System.Drawing.Point(0, 24);
             this.ChildTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ChildTextBox.Name = "ChildTextBox";
-            this.ChildTextBox.Size = new System.Drawing.Size(700, 314);
+            this.ChildTextBox.Size = new System.Drawing.Size(747, 348);
             this.ChildTextBox.TabIndex = 0;
             this.ChildTextBox.Text = "";
             // 
@@ -50,11 +56,12 @@ namespace MdiApplication
             // 
             this.ChildWindowMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.ChildWindowMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.ТoggleMenuItem});
+            this.ТoggleMenuItem,
+            this.editToolStripMenuItem});
             this.ChildWindowMenu.Location = new System.Drawing.Point(0, 0);
             this.ChildWindowMenu.Name = "ChildWindowMenu";
             this.ChildWindowMenu.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
-            this.ChildWindowMenu.Size = new System.Drawing.Size(700, 24);
+            this.ChildWindowMenu.Size = new System.Drawing.Size(747, 24);
             this.ChildWindowMenu.TabIndex = 1;
             this.ChildWindowMenu.Text = "menuStrip1";
             // 
@@ -72,15 +79,62 @@ namespace MdiApplication
             // formatToolStripMenuItem1
             // 
             this.formatToolStripMenuItem1.Name = "formatToolStripMenuItem1";
-            this.formatToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.formatToolStripMenuItem1.Size = new System.Drawing.Size(174, 22);
             this.formatToolStripMenuItem1.Text = "&Toggle Foreground";
             this.formatToolStripMenuItem1.Click += new System.EventHandler(this.formatToolStripMenuItem1_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.cutToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.pastToolStripMenuItem,
+            this.selectToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "&Edit";
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.copyToolStripMenuItem.Text = "&Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // cutToolStripMenuItem
+            // 
+            this.cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            this.cutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cutToolStripMenuItem.Text = "&Cut";
+            this.cutToolStripMenuItem.Click += new System.EventHandler(this.cutToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deleteToolStripMenuItem.Text = "&Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // pastToolStripMenuItem
+            // 
+            this.pastToolStripMenuItem.Name = "pastToolStripMenuItem";
+            this.pastToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pastToolStripMenuItem.Text = "&Past";
+            this.pastToolStripMenuItem.Click += new System.EventHandler(this.pastToolStripMenuItem_Click);
+            // 
+            // selectToolStripMenuItem
+            // 
+            this.selectToolStripMenuItem.Name = "selectToolStripMenuItem";
+            this.selectToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.selectToolStripMenuItem.Text = "&Select";
+            this.selectToolStripMenuItem.Click += new System.EventHandler(this.selectToolStripMenuItem_Click);
             // 
             // ChildForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.ClientSize = new System.Drawing.Size(747, 372);
             this.Controls.Add(this.ChildTextBox);
             this.Controls.Add(this.ChildWindowMenu);
             this.MainMenuStrip = this.ChildWindowMenu;
@@ -100,5 +154,11 @@ namespace MdiApplication
         private System.Windows.Forms.MenuStrip ChildWindowMenu;
         private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem1;
         public System.Windows.Forms.ToolStripMenuItem ТoggleMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pastToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectToolStripMenuItem;
     }
 }
--- WinFormsApp9/ChildForm.cs
+++ WinFormsApp9/ChildForm.cs
@@ -10,6 +10,7 @@ namespace MdiApplication
 {
     public partial class ChildForm : Form
     {
+        private String BufferText;
         public ChildForm()
         {
             InitializeComponent();
@@ -37,6 +38,55 @@ namespace MdiApplication
         private void formatToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
+        }
+
+
+
+        public void Cut()
+        {
+            BufferText = ChildTextBox.SelectedText;
+            ChildTextBox.SelectedText = "";
+        }
+        public void Copy()
+        {
+            BufferText = ChildTextBox.SelectedText;
+        }
+        public void Delete()
+        {
+            ChildTextBox.SelectedText = "";
+            BufferText = "";
+        }
+        public void Paste()
+        {
+            ChildTextBox.SelectedText = BufferText;
+        }
+        public void SelectAll()
+        {
+            ChildTextBox.SelectAll();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Cut();
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Delete();
+        }
+
+        private void pastToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Paste();
+        }
+
+        private void selectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SelectAll();
+        }
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Copy();
         }
     }
 }
